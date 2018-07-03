2ccc9518754f9bff06a6b5c5f84b00ff70a7b8c9
Your Name <you@example.com>
2018-07-03 12:01:19 +0100

    Requirements

---
+++ src/RequirementsInline.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace	DorsetDigital\SilverstripeRequirements;
+
+use SilverStripe\View\Requirements;
+use SilverStripe\View\SSViewer;
+use SilverStripe\View\ThemeResourceLoader;
+
+class	RequirementsInline	{
+	//put your code here
+}
