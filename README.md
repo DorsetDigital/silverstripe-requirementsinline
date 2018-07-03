db451c2ef1e5d2137271fedf9e50dab81c5e236c
Tim Burt <tim@dorset-digital.net>
2018-07-03 13:19:13 +0100

    Main functionality

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # silverstripe-requirementsinline
 Extension of SilverStripe's requirements to allow for adding scripts and CSS inline from files
+
+Silverstripe provides a number of ways to add Javascript and CSS to your pages, but currently doesn't allow you to add inline code from external files.
+This module is a simple way to do this, meaning that you can keep your inline scripts and CSS in separate files to help with your build process, but take advantage of using them inline where needed.
+
+# Requirements
+* Silverstripe 4.x
+
+# Installation
+* Install the code with `composer require dorsetdigital/silverstripe-requirementsinline`
+* Run a `dev/build?flush` to update your project
+
+# Usage
+
+Use the methods in the same way as the standard `Requirements` methods for `themedCSS()` and `themedJavascript()`:
+
+
+```php
+<?php
+
+use DorsetDigital\SilverstripeRequirements\RequirementsInline;
+
+class MyPageController extends PageController
+{
+
+ public function init()
+ {
+  parent::init();
+  RequirementsInline::themedJavascript('homescript');
+  RequirementsInline::themedCSS('homestyles');
+ }
+}
+```
--- src/RequirementsInline.php
+++ src/RequirementsInline.php
@@ -1,11 +1,33 @@
 <?php
-
-namespace	DorsetDigital\SilverstripeRequirements;
+namespace DorsetDigital\SilverstripeRequirements;
 
 use SilverStripe\View\Requirements;
 use SilverStripe\View\SSViewer;
 use SilverStripe\View\ThemeResourceLoader;
+use SilverStripe\Control\Director;
 
-class	RequirementsInline	{
-	//put your code here
+class RequirementsInline
+{
+
+ public static function themedJavascript($file, $uniqueId = null)
+ {
+  $path = ThemeResourceLoader::inst()->findThemedJavascript($file, SSViewer::get_themes());
+  if ($path) {
+   $script = file_get_contents(Director::baseFolder() . DIRECTORY_SEPARATOR . $path);
+   Requirements::customScript($script, $uniqueId);
+  } else {
+   Requirements::customScript('// ' . $file . ' not found');
+  }
+ }
+
+ public static function themedCSS($file, $uniqueId = null)
+ {
+  $path = ThemeResourceLoader::inst()->findThemedCSS($file, SSViewer::get_themes());
+  if ($path) {
+   $styles = file_get_contents(Director::baseFolder() . DIRECTORY_SEPARATOR . $path);
+   Requirements::customCSS($styles, $uniqueId);
+  } else {
+   Requirements::customCSS('/* ' . $file . ' not found */');
+  }  
+ }
 }
